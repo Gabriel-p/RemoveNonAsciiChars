3fb31ecfad5856ed999cfd97135c17637116106f
Gabriel-p <gabrielperren@gmail.com>
2017-01-29 12:01:25 -0300

    add files

---
+++ RemoveNonAscii.py
@@ -0,0 +1,9 @@
+import sublime
+import sublime_plugin
+import unicodedata
+
+class RemoveNonAsciiCharsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        entire_view = sublime.Region(0, self.view.size())
+        ascii_only = unicodedata.normalize('NFKD', self.view.substr(entire_view)).encode('ascii', 'ignore').decode('utf-8')
+        self.view.replace(edit, entire_view, ascii_only)
